8abf53e17e3277c9de7bc82abcc8a8be64dac305
Sergio Morales <s.mora.94@gmail.com>
2015-04-29 14:25:13 -0400

    Contact Us Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get '/contact_us' => 'home#contact_us'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
