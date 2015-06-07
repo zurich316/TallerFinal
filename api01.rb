e788380ae60917cf8d4fe554daeae0408cdc6fa8
Alejandro <carlos05.ale@gmail.com>
2015-06-07 17:40:06 -0400

    GOALS READY

--- api01.rb
+++ api01.rb
@@ -10,8 +10,8 @@ agent.add_auth('http://localhost:3000','sergio@fitrack.com','12345678')
 
 flag=0
 while flag<=180 do	
-	steps=Random.new.rand(0..30)
-	calories=Random.new.rand(0..15)
+	steps=Random.new.rand(0..100)
+	calories=Random.new.rand(0..50)
 	long=Random.new.rand(-17.399343..-17.366742)
 	lat=Random.new.rand(-66.180571..-66.136969)
 	#Asociamos los datos de la banda a la cuenta de usuario 1 (sergio@fitrack.com) y a su banda asociada con id 1.
