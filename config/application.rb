030fba8f7d2dc010754c59b327dbc97d9f33173e
Sergio Morales <s.mora.94@gmail.com>
2015-06-21 18:32:21 -0400

    assets

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module TallerProject
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.serve_static_assets = true
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.to_prepare do
